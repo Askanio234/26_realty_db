f628627fdc8392c7fcafde46ba9cb5cbd5fb4a0d
Artem Chernyaev <askanio234@gmail.com>
2017-05-15 20:50:23 +0300

    Add basic db model

---
+++ db_schema.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Boolean, Text, Date, Float
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('sqlite:///db_reality.sqlite')
+
+db_session = scoped_session(sessionmaker(bind=engine))
+
+data_base = declarative_base()
+
+data_base.query = db_session.query_property()
+
+
+class Residences(data_base):
+    __tablename__ = 'dwellings'
+    id = Column(Integer, primary_key=True)
+    settlement = Column(String(150), index=True)
+    is_under_construction = Column(Boolean)
+    description = Column(Text)
+    price = Column(Integer, index=True)
+    living_area = Column(Float)
+    premise_area = Column(Float)
+    has_balcony = Column(Boolean)
+    address = Column(Text)
+    construction_year = Column(Date)
+    rooms_number = Column(Integer)
+    work_id = Column(Integer)
+
+    def __str__(self):
+        return "residence in {} with work id - {}".format(
+                                            self.settlement,
+                                            self.work_id)
+
+
+if __name__ == '__main__':
+    data_base.metadata.drop_all(bind=engine)
+    data_base.metadata.create_all(bind=engine)
