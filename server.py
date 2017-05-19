b7439a07dca56f5f183d6b0ff27d4c7625591faf
Artem Chernyaev <askanio234@gmail.com>
2017-05-19 19:37:55 +0300

    Implement pagination for search results

--- server.py
+++ server.py
@@ -1,32 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from db_schema import db, Residences
 
 
 app = Flask(__name__)
 
+app.secret_key = "verysecretkeyexample"
+
 PER_PAGE = 15
 
-@app.route('/<int:page>')
+
 @app.route('/')
+@app.route('/<int:page>')
 def ads_list(page=1):
-    query = Residences.query.filter(Residences.is_active == True).paginate(page, PER_PAGE)
+    query = Residences.query.filter(
+                                Residences.is_active == True
+                                ).paginate(page, PER_PAGE)
     return render_template('ads_list.html', ads=query)
 
 
-@app.route('/search/')
-def filter_list():
-    settlement = request.args.get('oblast_district')
-    user_min_price = request.args.get('min_price')
-    user_max_price = request.args.get('max_price')
-    is_new_building = request.args.get('new_building', False)
-    queries = [Residences.settlement == settlement]
-    if user_min_price:
-        queries.append(Residences.price >= int(user_min_price))
-    if user_max_price:
-        queries.append(Residences.price <= int(user_max_price))
-    if is_new_building:
+@app.route('/search', methods=['POST'])
+def search():
+    session['region'] = request.form['oblast_district']
+    session['min_price'] = request.form.get('min_price',
+                                            None, type=int)
+    session['max_price'] = request.form.get('max_price',
+                                            None, type=int)
+    session['is_new'] = request.form.get('new_building',
+                                            False, type=bool)
+    return redirect(url_for('filter_list'))
+
+
+@app.route('/results/')
+@app.route('/results/<int:page>')
+def filter_list(page=1):
+    queries = [Residences.settlement == session['region']]
+    if session['min_price']:
+        queries.append(Residences.price >= session['min_price'])
+    if session['max_price']:
+        queries.append(Residences.price <= session['max_price'])
+    if session['is_new']:
         queries.append(Residences.is_new == True)
-    filtered_list = db.query(Residences).filter(*queries).all()
+    filtered_list = Residences.query.filter(*queries).paginate(page,
+                                                    PER_PAGE, False)
 
     return render_template('ads_list.html', ads=filtered_list)
 
