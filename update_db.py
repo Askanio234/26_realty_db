d1830e4e7f7a5c2679877515834ff6e43e0fe27c
Artem Chernyaev <askanio234@gmail.com>
2017-05-23 21:10:47 +0300

    Complete rewrite of logic for updating the database - with less queries

--- update_db.py
+++ update_db.py
@@ -29,43 +29,69 @@ def check_is_a_new(item):
             (TODAY - (item["construction_year"] or 0) <= YEARS_NEW)
 
 
-def update_or_create_residences(json_data):
+def get_inner_corp_id_list(json_data):
+    list_of_inner_corp_ids = []
     for item in json_data:
-        work_id = item["id"]
-        residence = Residences.query.filter(
-                                    Residences.work_id == work_id
-                                                        ).first()
-        if residence is not None:
-            residence.is_active = True
-        else:
-            residence_to_add = Residences(
+        list_of_inner_corp_ids.append(item["id"])
+    return list_of_inner_corp_ids
+
+
+def update_residence_row(model, json_dict):
+    for key, value in json_dict.items():
+        if hasattr(model, key):
+            if key != "id":
+                setattr(model, key, value)
+                setattr(model, 'active', True)
+                model.is_new = check_is_a_new(json_dict)
+
+
+def update_residences(json_data, inner_corp_ids):
+    query = Residences.query.filter(
+                            Residences.inner_corp_id.in_(inner_corp_ids))
+    for residence in query:
+        for item in json_data:
+            if residence.inner_corp_id == item["id"]:
+                update_residence_row(residence, item)
+    db.session.commit()
+
+
+def create_new_residences(json_data, inner_corp_ids):
+    inner_corp_id_in_db = [item[0] for item in
+                            db.session.query(Residences.inner_corp_id)]
+    new_residences_corp_id = list(set(inner_corp_ids) -
+                                    set(inner_corp_id_in_db))
+    for item in json_data:
+        for new_id in new_residences_corp_id:
+            if item["id"] == new_id:
+                residence_to_add = Residences(
                 settlement=item["settlement"],
                 is_under_construction=item["under_construction"],
                 description=item["description"],
                 price=item["price"],
-                living_area=item["living_area"],
                 premise_area=item["premise_area"],
-                has_balcony=item["has_balcony"],
                 address=item["address"],
                 construction_year=item["construction_year"],
                 rooms_number=item["rooms_number"],
-                work_id=item["id"],
+                inner_corp_id=item["id"],
                 is_new=check_is_a_new(item),
                 is_active=True
                 )
-            db.session.add(residence_to_add)
+                db.session.add(residence_to_add)
     db.session.commit()
 
 
-def disable_old_data():
-    all_residences = Residences.query.all()
-    if all_residences is not None:
-        for residence in all_residences:
-            residence.is_active = False
-        db.session.commit()
+def disable_old_data(inner_corp_ids):
+    query = Residences.query.filter(
+                        Residences.inner_corp_id.notin_(inner_corp_ids))
+    for residence in query:
+        residence.is_active = False
+    db.session.commit()
 
 
 if __name__ == '__main__':
     args = get_args()
     json_file = load_data(args.input)
-    update_or_create_residences(json_file)
+    list_of_inner_ids = get_inner_corp_id_list(json_file)
+    update_residences(json_file, list_of_inner_ids)
+    create_new_residences(json_file, list_of_inner_ids)
+    disable_old_data(list_of_inner_ids)
